bf427bc247c1501cc5c522a7483df8a38996405b
nadya_bryn <nadya.bryn@gmail.com>
2019-12-30 10:32:44 +0200

    edited Payment table

--- Tables/dbo.DimPayments.sql
+++ Tables/dbo.DimPayments.sql
@@ -2,8 +2,7 @@ IF OBJECT_ID('dbo.DimPayments', 'U') IS NOT NULL
   DROP TABLE dbo.DimPayments; 
 
 CREATE TABLE DimPayments(
- PaymentID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
- PaymentType VARCHAR(50),
- StatusPayment BIT,
- PaymentDescription VARCHAR(200)
+   PaymentID INT NOT NULL PRIMARY KEY IDENTITY(1,1)
+ , PaymentType VARCHAR(50)
+ , PaymentDescription VARCHAR(200)
 )
