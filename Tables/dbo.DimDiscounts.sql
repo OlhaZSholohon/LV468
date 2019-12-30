da71dbeb96f54a16bbc528318e7aaa5e0adb9f0f
igor_bomz <igor.shvidyuk@gmail.com>
2019-12-30 14:33:49 +0200

    Push tables Clients and Discount

---
+++ Tables/dbo.DimDiscounts.sql
@@ -0,0 +1,10 @@
+﻿IF OBJECT_ID('dbo.DimDiscounts') IS NOT NULL
+	DROP TABLE dbo.DimDiscounts
+
+CREATE TABLE DimDiscounts(
+ DiscountID INT NOT NULL PRIMARY KEY IDENTITY(1,1)
+ , StartDiscount DATE
+ , EndDiscount DATE
+ , PercentDiscount INT
+ , DescriptionDiscount VARCHAR(200)
+)
