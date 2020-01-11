49a8945e5db690fe4a94fb48fff9c2a0e91bbaa0
Artur Tymoshenko <atymostc@training.local>
2020-01-11 10:37:02 +0200

    Add DimClients table

---
+++ StagingTables/DimClients.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE [stage].[DimClients](
+	[ClientID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](255) NULL,
+	[LastName] [nvarchar](255) NULL,
+	[Gender] [nvarchar](255) NULL,
+	[Birthday] [nvarchar] (255) NULL,
+	[Email] [nvarchar](255) NULL,
+	[Phone] [nvarchar](255) NULL,
+	[UserLogin] [nvarchar](255) NULL,
+	[LoginPassword] [nvarchar](255) NULL,
+	[RegistrationDate] [nvarchar] (255) NULL,
+) 
+ON [PRIMARY]
+GO
+
+
