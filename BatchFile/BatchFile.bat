03c084063893fe7f8eb89af0b8184aaf2be208b9
AndriiTybinka <andriy.tybinka@gmail.com>
2020-01-13 15:38:00 +0200

    modified SSIS project

--- BatchFile/BatchFile.bat
+++ BatchFile/BatchFile.bat
@@ -3,8 +3,8 @@
 REM Please, change the path where batch is sourced.
 REM Batch file does sql script in files which consist only in the same folder
 
-pushd \\fstrain\Exchange\Lv-468.DB\StagingTables\1
+pushd H:\LV468\StagingTables
 
-FOR %%A IN (*.SQL) DO ( sqlcmd -S "192.168.195.147" -d "test2_ihor" -U "Lv-468.DB" -P "Lv-468.DB" -i "%%A" -I )
+FOR %%A IN (*.SQL) DO ( sqlcmd -S "192.168.195.147" -d "TestDBStage" -U "Lv-468.DB" -P "Lv-468.DB" -i "%%A" -I )
 
 popd
