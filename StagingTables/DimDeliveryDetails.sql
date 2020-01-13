d58eb9a8ea3466a0818b22cf5e05d4510965a3fc
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:23:44 +0200

    Edited table DimDeliveryDetails

--- StagingTables/DimDeliveryDetails.sql
+++ StagingTables/DimDeliveryDetails.sql
@@ -1,12 +1,7 @@
-USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimDeliveryDetails]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+USE [TestDBStage];
+DROP TABLE IF EXISTS [staging].[DimDeliveryDetails];
+
 CREATE TABLE [staging].[DimDeliveryDetails](
 	[DeliveryDetailID] [int] IDENTITY(1,1) NOT NULL,
 	[StatusDelivery] [nvarchar](255) NULL
-) ON [PRIMARY]
-GO
+) 
