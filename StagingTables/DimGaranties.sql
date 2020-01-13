a951c95647c7c9962f5b3e05ec68a361ff132182
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:30:24 +0200

    Edited table DimGaranties

--- StagingTables/DimGaranties.sql
+++ StagingTables/DimGaranties.sql
@@ -1,15 +1,9 @@
-USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimGaranties]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+USE [TestDBStage];
+DROP TABLE IF EXISTS [staging].[DimGaranties];
 CREATE TABLE [staging].[DimGaranties](
 	[GarantyID] [int] IDENTITY(1,1) NOT NULL,
 	[NameGaranty] [nvarchar](255) NULL,
 	[Duration] [nvarchar](255) NULL,
 	[PriceGaranty] [nvarchar](255) NULL,
 	[DescriptionGaranty] [nvarchar](255) NULL
-) ON [PRIMARY]
-GO
+) 
