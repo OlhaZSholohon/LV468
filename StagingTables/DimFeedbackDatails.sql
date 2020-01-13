0c1f64b175bbc466760f138778229f3fd7d2270b
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:28:26 +0200

    Edited table DimFeedbackDetails

--- StagingTables/DimFeedbackDatails.sql
+++ StagingTables/DimFeedbackDatails.sql
@@ -1,10 +1,5 @@
-USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimFeedbackDatails]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+USE [TestDBStage];
+DROP TABLE IF EXISTS [staging].[DimFeedbackDatails];
 CREATE TABLE [staging].[DimFeedbackDatails](
 	[FeedbackDetailID] [int] IDENTITY(1,1) NOT NULL,
 	[Comment] [nvarchar](255) NULL,
