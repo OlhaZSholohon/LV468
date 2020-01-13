aaf3a42f87b336f385cab5930fbb3ea528a73096
AndriiTybinka <andriy.tybinka@gmail.com>
2020-01-13 12:37:13 +0200

    Changed SP_DimGaranties

--- SP/SP_Populate_DimGaranties.sql
+++ SP/SP_Populate_DimGaranties.sql
@@ -1,17 +1,17 @@
---USE TestNoFK;
+--USE TestDBStage;
 
-/*IF OBJECT_ID('dbo.DimGaranties') IS NOT NULL
-	DROP TABLE dbo.DimGaranties
-CREATE TABLE DimGaranties(
-   GarantyID INT NOT NULL PRIMARY KEY IDENTITY(1,1)
- , NameGaranty VARCHAR(50)
- , Duration INT
- , PriceGaranty INT
- , DescriptionGaranty VARCHAR(200)
+/*IF OBJECT_ID('staging.DimGaranties') IS NOT NULL
+	DROP TABLE staging.DimGaranties
+CREATE TABLE staging.DimGaranties(
+   GarantyID INT NOT NULL IDENTITY(1,1)
+ , NameGaranty NVARCHAR(255)
+ , Duration NVARCHAR(255)
+ , PriceGaranty NVARCHAR(255)
+ , DescriptionGaranty NVARCHAR(MAX)
 )
 */
 
-
+USE TestDBStage;
 GO
 --exec SP_PopulateDimGaranties 100;
 
@@ -41,7 +41,7 @@ union all
 SELECT 'Additional reseller''s warranty' AS NameGaranty, 'Some description of warranty - 4' AS DescriptionGaranty
 )
 
-INSERT INTO DimGaranties (NameGaranty, Duration, PriceGaranty, DescriptionGaranty)
+INSERT INTO staging.DimGaranties (NameGaranty, Duration, PriceGaranty, DescriptionGaranty)
 SELECT  c1.NameGaranty 
 		, round( rand()*200, 0)
 		, round( rand()*400, 0)
@@ -53,10 +53,10 @@ CTE_TempDictionary c2
 -------------------------------
 SELECT * 
 INTO #TempDimGaranties
-FROM DimGaranties
+FROM staging.DimGaranties
 -------------------------------
 
-SELECT @InsertedRows = @@ROWCOUNT
+SET @InsertedRows = @@ROWCOUNT
 
 SET @RowsForWhile = @NumberOfRows - @InsertedRows
 
@@ -64,7 +64,7 @@ SET @RowsForWhile = @NumberOfRows - @InsertedRows
 WHILE @Loop <= @RowsForWhile
 BEGIN 
 	SET @WarrantyRandName = (SELECT TOP 1 NameGaranty FROM #TempDimGaranties ORDER BY NEWID())
-  INSERT INTO DimGaranties (NameGaranty, Duration, PriceGaranty, DescriptionGaranty)
+  INSERT INTO staging.DimGaranties (NameGaranty, Duration, PriceGaranty, DescriptionGaranty)
   VALUES (
 		@WarrantyRandName,
 		round(rand()*150, 0),
