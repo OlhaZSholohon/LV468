b3324537a81752ba9fa016ee8425c535a3ebf6ff
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:36:56 +0200

    Edited table DimReturnDetails

--- StagingTables/DimReturnDetails.sql
+++ StagingTables/DimReturnDetails.sql
@@ -1,13 +1,7 @@
 USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimReturnDetails]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+DROP TABLE IF EXISTS [staging].[DimReturnDetails];
 CREATE TABLE [staging].[DimReturnDetails](
 	[ReturnDetailID] [int] IDENTITY(1,1) NOT NULL,
 	[ReturnKind] [nvarchar](255) NULL,
 	[ReturnDescription] [nvarchar](255) NULL
-) ON [PRIMARY]
-GO
+) 
