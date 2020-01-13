05d3c1a18f20b7c4c072e1974411b8f7d71b9d4f
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:34:08 +0200

    Edited table DimPayments

--- StagingTables/DimPayments.sql
+++ StagingTables/DimPayments.sql
@@ -1,13 +1,7 @@
 USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimPayments]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+DROP TABLE IF EXISTS [staging].[DimPayments];
 CREATE TABLE [staging].[DimPayments](
 	[PaymentID] [int] IDENTITY(1,1) NOT NULL,
 	[PaymentType] [nvarchar](255) NULL,
 	[PaymentDescription] [nvarchar](255) NULL
-) ON [PRIMARY]
-GO
+) 
