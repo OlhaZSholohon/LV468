f749c3ce87c4eec14ff2caa0415eb6d260edabdd
i_shvydiuk <igor.shvidyuk@gmail.com>
2020-01-13 23:46:52 +0200

    Modified ST to populate DimSuppliers and added SSIS package for the table

--- SP/SP_Populate_DimSuppliers.sql
+++ SP/SP_Populate_DimSuppliers.sql
@@ -1,3 +1,15 @@
+USE [TestDBStage]
+
+IF OBJECT_ID('staging.DimSuppliers', 'U') IS NOT NULL 
+DROP TABLE [staging].DimSuppliers;
+CREATE TABLE [staging].DimSuppliers(
+	[SupplierID] [int] IDENTITY(1,1) NOT NULL,
+	[SupplierName] [nvarchar](255) NULL,
+	[SupplierEmail] [nvarchar](255) NULL,
+	[SupplierPhone] [nvarchar](255) NULL,
+	[SupplierDescription] [nvarchar](255) NULL
+) 
+
 CREATE OR ALTER PROCEDURE staging.SP_PopulateDimSuppliers
 	@NumberOfRows INT
 AS
