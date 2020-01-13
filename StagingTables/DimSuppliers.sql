bd3c94e519575a7c985a455261c3b228cee84c16
nadya_bryn <nadya.bryn@gmail.com>
2020-01-13 11:38:22 +0200

    Edited table DimSuppliers

--- StagingTables/DimSuppliers.sql
+++ StagingTables/DimSuppliers.sql
@@ -1,15 +1,9 @@
 USE [TestDBStage]
-GO
-/****** Object:  Table [staging].[DimSuppliers]    Script Date: 09-Jan-20 13:30:10 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+DROP TABLE IF EXISTS [staging].[DimSuppliers];
 CREATE TABLE [staging].[DimSuppliers](
 	[SupplierID] [int] IDENTITY(1,1) NOT NULL,
 	[SupplierName] [nvarchar](255) NULL,
 	[SupplierEmail] [nvarchar](255) NULL,
 	[SupplierPhone] [nvarchar](255) NULL,
 	[SupplierDescription] [nvarchar](255) NULL
-) ON [PRIMARY]
-GO
+) 
