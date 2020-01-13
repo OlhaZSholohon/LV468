a4a9f5dd5f6e44083eade38b5e43634708b447b8
AndriiTybinka <andriy.tybinka@gmail.com>
2020-01-13 14:26:51 +0200

    Modified bat-file

--- StagingTables/BatchFile.bat
+++ StagingTables/BatchFile.bat
@@ -3,7 +3,7 @@
 REM Please, change the path where batch is sourced.
 REM Batch file does sql script in files which consist only in the same folder
 
-pushd H:\LVDB468\StagingTables
+pushd H:\LV468\StagingTables
 
 FOR %%A IN (*.SQL) DO ( sqlcmd -S "192.168.195.147" -d "TestDBStage" -U "Lv-468.DB" -P "Lv-468.DB" -i "%%A" -I )
 
