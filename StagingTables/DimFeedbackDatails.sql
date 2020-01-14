216c86e8937b021d1841a05d5a0eec326244c194
AndriiTybinka <andriy.tybinka@gmail.com>
2020-01-14 02:15:30 +0200

    Updated SSIS project Lading_Data_into_StageDBTest

--- StagingTables/DimFeedbackDatails.sql
+++ StagingTables/DimFeedbackDatails.sql
@@ -1,10 +1,10 @@
 USE TestDBStage;
 
-IF OBJECT_ID('staging.DimFeedbackDatails') IS NOT NULL
-	DROP TABLE staging.DimFeedbackDatails
+IF OBJECT_ID('staging.DimFeedbackDetails') IS NOT NULL
+	DROP TABLE staging.DimFeedbackDetails
 
 
-CREATE TABLE [staging].[DimFeedbackDatails](
+CREATE TABLE [staging].[DimFeedbackDetails](
 	[FeedbackDetailID] [int] IDENTITY(1,1) NOT NULL,
 	[Comment] [nvarchar](255) NULL,
 	[Rating] [nvarchar](255) NULL
