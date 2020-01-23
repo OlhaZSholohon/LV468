0926447c835bb7dffed79d06839fd8e3c9daf58c
i_shvydiuk <igor.shvidyuk@gmail.com>
2020-01-23 13:19:55 +0200

    Modified table DimSuppliers in DB TestDBDataMart

--- DMTables/DimSuppliers.sql
+++ DMTables/DimSuppliers.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS [datamart].[DimSuppliers];
 
 CREATE TABLE [datamart].[DimSuppliers](
 	[SupplierID] [int] IDENTITY(1,1) NOT NULL,
-	[SupplierName] [nvarchar](100) NULL,
-	[SupplierEmail] [nvarchar](100) NULL,
+	[SupplierName] [nvarchar](50) NULL,
+	[SupplierEmail] [nvarchar](50) NULL,
 	[SupplierPhone] [nvarchar](20) NULL,
 	[SupplierDescription] [nvarchar](255) NULL
 ) 
