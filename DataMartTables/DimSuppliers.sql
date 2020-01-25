1b4551e40a8ad38ef81020963d9f641b1997b194
i_shvydiuk <igor.shvidyuk@gmail.com>
2020-01-25 19:36:39 +0200

    Modified query for creating DimSuppliers Table

--- DataMartTables/DimSuppliers.sql
+++ DataMartTables/DimSuppliers.sql
@@ -3,9 +3,12 @@ IF OBJECT_ID('[datamart].[DimSuppliers]', 'U') IS NOT NULL
   DROP TABLE [datamart].[DimSuppliers]; 
 
 CREATE TABLE [datamart].[DimSuppliers](
-	[SupplierID] [int] IDENTITY(1,1) NOT NULL,
-	[SupplierName] [nvarchar](50) NULL,
-	[SupplierEmail] [nvarchar](50) NULL,
-	[SupplierPhone] [nvarchar](20) NULL,
-	[SupplierDescription] [nvarchar](255) NULL
+	[SupplierID] [int] IDENTITY(1,1) NOT NULL
+	,[SupplierKey] INT
+	,[SupplierName] [nvarchar](20) NULL
+	,[SupplierEmail] [nvarchar](40) NULL
+	,[SupplierPhone] [nvarchar](18) NULL
+	,[SupplierDescription] [nvarchar](255) NULL
+	,ValidDateFrom DATETIME NOT NULL
+	,ValidateTo DATETIME NULL
 ) 
