4324612b5c1f1eaa150391fe9b9fbe9ba7ef1e30
Misha <7mishakarpa7@gmail.com>
2020-02-05 16:04:54 +0200

    Update DimLocation

--- DataMartTables/DimLocations.sql
+++ DataMartTables/DimLocations.sql
@@ -4,10 +4,10 @@ IF OBJECT_ID('[datamart].[DimLocations]', 'U') IS NOT NULL
 
 CREATE TABLE [datamart].[DimLocations](
 	[LocationID] [int] IDENTITY(1,1) NOT NULL,
-	[Country] [nvarchar](50) NULL,
-	[Region] [nvarchar](100) NULL,
-	[City] [nvarchar](100) NULL,
-	[Adress] [nvarchar](100) NULL,
-	[PostNumber] [int] NULL,
+	[Country] [nvarchar](45) NULL,
+	[Region] [nvarchar](50) NULL,
+	[City] [nvarchar](50) NULL,
+	[Adress] [nvarchar](50) NULL,
+	[PostNumber] [nvarchar](6) NULL,
 	[DeliveryService] [nvarchar](50) NULL
 ) 
