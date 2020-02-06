638767e295d10436ac874d6bd38fb7331278caee
AndriiTybinka <andriy.tybinka@gmail.com>
2020-02-06 17:19:40 +0200

    1. Added separate package DatamartFactOrders 2. Updateted SP_PopulateFactOrders

--- SP/Demo_2/SP_PopulateFactOrders.sql
+++ SP/Demo_2/SP_PopulateFactOrders.sql
@@ -33,14 +33,14 @@ BEGIN
 	SET @Date = (SELECT TOP 1 [Date] FROM [TestDBDataMart].[datamart].[DimDates] ORDER BY NEWID())
 	SET @PaymentID = (SELECT TOP 1 [PaymentTypeID] FROM [TestDBDataMart].[datamart].[DimPayments] ORDER BY NEWID())
 	SET @GarantyID = (SELECT TOP 1 [GarantyID] FROM [TestDBDataMart].[datamart].[DimGaranties] g ORDER BY NEWID())
-	SET @ClientID = (SELECT TOP 1 [ClientID] FROM [TestDBStage].[staging].[DimClients] ORDER BY NEWID())
-	SET @DiscountID = (SELECT TOP 1 [DiscountID] FROM [TestDBStage].[staging].[DimDiscounts] ORDER BY NEWID())
+	SET @ClientID = (SELECT TOP 1 [ClientID] FROM [TestDBDataMart].[datamart].[DimClients] ORDER BY NEWID())
+	SET @DiscountID = (SELECT TOP 1 [DiscountID] FROM [TestDBDataMart].[datamart].[DimDiscounts] ORDER BY NEWID())
 	SET @ProductID = (SELECT TOP 1 [ProductID] FROM [TestDBDataMart].[datamart].[DimProducts] ORDER BY NEWID())
 	SET @AmountProducts = round(rand()*120, 0)
 	
 	--------- Addition variables -----------------
-	SET @GarantyPrice = TRY_CAST((SELECT g.PriceGaranty  FROM [TestDBDataMart].[datamart].[DimGaranties] g WHERE g.GarantyID = @GarantyID AND g.EndDate IS NULL) AS DECIMAL (18, 2))
-	SET @ProductPrice = TRY_CAST((SELECT TOP 1 p.Price FROM [TestDBDataMart].[datamart].[DimProducts] p WHERE p.ProductID = @ProductID) AS DECIMAL (18, 2))
+	SET @GarantyPrice = TRY_CAST((SELECT g.PriceGaranty  FROM [TestDBDataMart].[datamart].[DimGaranties] g WHERE g.GarantyID = @GarantyID AND g.EndDate IS NULL) AS DECIMAL (10, 2))
+	SET @ProductPrice = TRY_CAST((SELECT p.Price FROM [TestDBDataMart].[datamart].[DimProducts] p WHERE p.ProductID = @ProductID) AS DECIMAL (10, 2))
 	SET @Discount = TRY_CAST((SELECT d.PercentDiscount FROM [TestDBDataMart].[datamart].[DimDiscounts] d WHERE d.DiscountID = @DiscountID) AS INT)
 	SET @GarantyDuration = TRY_CAST((SELECT g.Duration  FROM [TestDBDataMart].[datamart].[DimGaranties] g WHERE g.GarantyID = @GarantyID AND g.EndDate IS NULL) AS INT)
 
@@ -64,8 +64,8 @@ BEGIN
 		, @DiscountID
 		, @ProductID
 		, @AmountProducts
-		, TRY_CAST((@AmountProducts * @ProductPrice + @GarantyPrice) AS DECIMAL (18,2))
-		, TRY_CAST(((@AmountProducts * @ProductPrice + @GarantyPrice) * @Discount)/100 as decimal (18, 2))
+		, TRY_CAST((@AmountProducts * @ProductPrice + @GarantyPrice) AS DECIMAL (10,2))
+		, TRY_CAST(((@AmountProducts * @ProductPrice + @GarantyPrice) * @Discount *0.1)/100 as decimal (10, 2))
 		, DATEADD (DAY, @GarantyDuration, @Date)
 		)
 
