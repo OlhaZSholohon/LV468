0bec3f14df2021cb44f17be3bd98d2ad0c795379
mark10s <markiyanskolozdriy@gmail.com>
2020-02-07 16:07:10 +0200

    update create table script for DimDeliveryDetails

--- DataMartTables/DimDeliveryDetails.sql
+++ DataMartTables/DimDeliveryDetails.sql
@@ -2,7 +2,21 @@ USE [TestDBDataMart];
 IF OBJECT_ID('[datamart].[DimDeliveryDetails]', 'U') IS NOT NULL 
   DROP TABLE [datamart].[DimDeliveryDetails]; 
 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE TABLE [datamart].[DimDeliveryDetails](
-	[DeliveryDetailID] [int] IDENTITY(1,1) NOT NULL,
-	[StatusDelivery] [bit] NULL
-) 
+	[DeliveryDetailID] [int] NULL,
+	[StatusDelivery] [bit] NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedDate] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [datamart].[DimDeliveryDetails] ADD  DEFAULT (getdate()) FOR [CreatedDate]
+GO
+
+
