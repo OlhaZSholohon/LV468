ec7f56f21a86dd8f4aa811483561128c6ef64464
mark10s <markiyanskolozdriy@gmail.com>
2020-02-07 16:06:00 +0200

    update create table script for DimReturnDetails

--- DataMartTables/DimReturnDetails.sql
+++ DataMartTables/DimReturnDetails.sql
@@ -1,9 +1,22 @@
 USE [TestDBDataMart]
+
 IF OBJECT_ID('[datamart].[DimReturnDetails]', 'U') IS NOT NULL 
   DROP TABLE [datamart].[DimReturnDetails]; 
 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE TABLE [datamart].[DimReturnDetails](
-	[ReturnDetailID] [int] IDENTITY(1,1) NOT NULL,
-	[ReturnKind] [nvarchar](50) NULL,
-	[ReturnDescription] [nvarchar](255) NULL
-) 
+	[ReturnDetailID] [int] NOT NULL,
+	[ReturnKind] [nvarchar](30) NULL,
+	[ReturnDescription] [nvarchar](255) NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedDate] [datetime] NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [datamart].[DimReturnDetails] ADD  DEFAULT (getdate()) FOR [CreatedDate]
+GO
