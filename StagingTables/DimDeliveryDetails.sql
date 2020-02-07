46156e3064d24dc3500615ced7f9e045be5ebfaa
mark10s <markiyanskolozdriy@gmail.com>
2020-02-07 16:47:06 +0200

    change script for create staging tables DimDelieryDetails and DimReturnDetails

--- StagingTables/DimDeliveryDetails.sql
+++ StagingTables/DimDeliveryDetails.sql
@@ -1,7 +1,9 @@
+USE [TestDBStage]
+
 IF OBJECT_ID('staging.DimDeliveryDetails') IS NOT NULL
 	DROP TABLE staging.DimDeliveryDetails
 
 CREATE TABLE [staging].[DimDeliveryDetails](
-	[DeliveryDetailID] [int] IDENTITY(1,1) NOT NULL,
+	[DeliveryDetailID] [nvarchar](255) NULL,
 	[StatusDelivery] [nvarchar](255) NULL
 ) 
