e984302dcf931d380f37344a84d444c041163dec
KuolemaZahar <zfs215@gmail.com>
2020-02-07 14:03:47 +0200

    Update for staging tables DImDiscounts and DimFeedbackDetails

--- StagingTables/DimDiscounts.sql
+++ StagingTables/DimDiscounts.sql
@@ -1,10 +1,24 @@
-IF OBJECT_ID('staging.DimDiscounts') IS NOT NULL
-	DROP TABLE staging.DimDiscounts
+USE [TestDBStage]
+GO
+
+/****** Object:  Table [staging].[DimDiscounts]    Script Date: 2/7/2020 1:58:20 PM ******/
+DROP TABLE [staging].[DimDiscounts]
+GO
+
+/****** Object:  Table [staging].[DimDiscounts]    Script Date: 2/7/2020 1:58:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE TABLE [staging].[DimDiscounts](
-	[DiscountID] [int] IDENTITY(1,1) NOT NULL,
+	[DiscountID] [int]  NULL,
 	[StartDiscount] [nvarchar](255) NULL,
 	[EndDiscount] [nvarchar](255) NULL,
 	[PercentDiscount] [nvarchar](255) NULL,
 	[DescriptionDiscount] [nvarchar](255) NULL
-) 
+) ON [PRIMARY]
+GO
+
+
--- StagingTables/DimFeedbackDatails.sql
+++ StagingTables/DimFeedbackDatails.sql
@@ -1,10 +1,22 @@
-IF OBJECT_ID('staging.DimFeedbackDetails') IS NOT NULL
-	DROP TABLE staging.DimFeedbackDetails
+USE [TestDBStage]
+GO
 
+/****** Object:  Table [staging].[DimFeedbackDetails]    Script Date: 2/7/2020 1:56:55 PM ******/
+DROP TABLE [staging].[DimFeedbackDetails]
+GO
+
+/****** Object:  Table [staging].[DimFeedbackDetails]    Script Date: 2/7/2020 1:56:55 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
 
 CREATE TABLE [staging].[DimFeedbackDetails](
-	[FeedbackDetailID] [int] IDENTITY(1,1) NOT NULL,
+	[FeedbackDetailID] [int] NULL,
 	[Comment] [nvarchar](255) NULL,
 	[Rating] [nvarchar](255) NULL
 ) ON [PRIMARY]
 GO
+
+
