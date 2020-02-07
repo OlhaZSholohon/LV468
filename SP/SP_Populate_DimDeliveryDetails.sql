8d5bc60d596ea14af3e78fe3461bcfbf4171ee30
mark10s <markiyanskolozdriy@gmail.com>
2020-02-07 16:55:25 +0200

    update procedure for populate staging table DimDeliveryDetails

--- SP/SP_Populate_DimDeliveryDetails.sql
+++ SP/SP_Populate_DimDeliveryDetails.sql
@@ -3,24 +3,24 @@ USE [TestDBStage]
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'staging.SP_Populate_DimDeliveryDetails') AND type in (N'P', N'PC'))
   DROP PROCEDURE [staging].[SP_Populate_DimDeliveryDetails]
 
---exec Staging.SP_Populate_DimDeliveryDetails 100
 GO
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [staging].[SP_Populate_DimDeliveryDetails]
-  @NumberOfRows INT
+CREATE PROCEDURE [staging].[SP_Populate_DimDeliveryDetails] 
+@NumberOfRows INT
 AS
 DECLARE @Loop INT;   
 SET @Loop = 1;
 WHILE @Loop <= @NumberOfRows
 BEGIN 
-INSERT INTO staging.DimDeliveryDetails (StatusDelivery)
+INSERT INTO staging.DimDeliveryDetails (DeliveryDetailID, StatusDelivery)
 VALUES ( 
-           round( rand()*1,0)
-        )
+		 round( rand()*3000,0),
+         round( rand()*1,0)
+       )
 SET @Loop = @Loop + 1 
 END
   
