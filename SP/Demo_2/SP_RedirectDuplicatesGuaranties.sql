a6ccbc1775a6d03c8677fd2d66e20f5ced8fc529
AndriiTybinka <andriy.tybinka@gmail.com>
2020-02-08 12:09:03 +0200

    Updated SSIS project DatamartDimGuaranties

---
+++ SP/Demo_2/SP_RedirectDuplicatesGuaranties.sql
@@ -0,0 +1,29 @@
+﻿--USE TestDBDataMart;
+
+SET NOCOUNT ON
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'datamart.SP_RedirectDuplicatesGuaranties') AND type in (N'P', N'PC'))
+  DROP PROCEDURE datamart.SP_RedirectDuplicatesGuaranties
+
+GO
+CREATE PROCEDURE datamart.SP_RedirectDuplicatesGuaranties
+AS
+-------INSERT------------
+SELECT GuarantyID, NameGuaranty, Duration, PriceGuaranty, DescriptionGuaranty
+INTO #GuarantyDuplicates
+FROM datamart.DimGuarantiesCastedData cd1
+WHERE (SELECT COUNT(*)
+       FROM datamart.DimGuarantiesCastedData cd2
+       WHERE cd1.GuarantyID = cd2.GuarantyID) > 1
+--------------------------
+
+INSERT INTO error.DimGuarantyErrors(GuarantyID, NameGuaranty, Duration, PriceGuaranty, DescriptionGuaranty, ErrorDescription, ErrorDate)
+SELECT GuarantyID, NameGuaranty, Duration, PriceGuaranty, DescriptionGuaranty, 'Error: duplicates in source file(s)', GETDATE() FROM #GuarantyDuplicates;
+
+-----------DELETE------
+DELETE cd 
+	FROM datamart.DimGuarantiesCastedData cd
+	INNER JOIN #GuarantyDuplicates td
+	ON cd.GuarantyID = td.GuarantyID
+
+GO
