bbcec233d7d8ca27303bfe144e75acab7b4442b6
arturtymoshenko1992 <arturtymoshenko92@gmail.com>
2020-03-01 14:40:25 +0200

    Add Views LocationsDimProducts_SCD

--- Views/dbo.vwReportLocationsDimProducts_SCD.sql
+++ Views/dbo.vwReportLocationsDimProducts_SCD.sql
@@ -1,33 +1,30 @@
 USE [TestDBDataMart]
 GO
 
-/****** Object:  View [dbo].[ReportLocationsDimLocations]    Script Date: 3/1/2020 1:09:04 PM ******/
+/****** Object:  View [dbo].[ReportLocationsDimProducts_SCD]    Script Date: 3/1/2020 2:36:52 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-CREATE view [dbo].[ReportLocationsDimLocations]
+Create view [dbo].[ReportLocationsDimProducts_SCD]
 AS
-SELECT [LocationID]
-      ,[Country]
-      ,[Region]
-	  ,SUBSTRING(Region, CHARINDEX('/', Region) + 1, LEN(Region)) AS Town
-	  ,case when Region like '%America%' then 'America'
-	        when Region like '%Asia%' then 'Asia'
-			when Region like '%Africa%' then 'Africa'
-			when Region like '%Europe%' then 'Europe'
-			Else Null 
-       End AS Continent
-      ,[City]
-      ,[Adress]
-      ,[PostNumber]
-      ,[DeliveryService]
-      ,[CreatedDate]
-      ,[UpdatedDate]
-  FROM [TestDBDataMart].[datamart].[DimLocations]
+SELECT  [ProductID]
+      ,[ProductName]
+      ,[CategoryName]
+      ,[SubcategoryName]
+      ,[BrandName]
+      ,[Thicknes]
+      ,[Weight]
+      ,[ProductDescription]
+      ,[DefaultGaranty]
+      ,[DefaultGarantyName]
+      ,[Price]
+      ,[StartDate]
+      ,[EndDate]
+  FROM [TestDBDataMart].[datamart].[DimProducts_SCD]
+  WHERE EndDate is Null
 GO
 
 
